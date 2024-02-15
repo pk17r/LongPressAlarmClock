6475999e32d833363ced1d4a01ae3dd62227932c
Prashant Kumar <prash.k20@gmail.com>
2024-02-15 05:34:47 -0800

    Now using external EEPROM

--- persistent_data.cpp
+++ persistent_data.cpp
@@ -1,212 +1,164 @@
 
 #include "persistent_data.h"
 
+persistent_data::persistent_data() {
+  eeprom.set_address(0x57);
+  Wire.begin();
+}
+
 
 bool persistent_data::retrieveAlarmSettings(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn) {
-  #if defined(MCU_IS_RASPBERRY_PI_PICO_W)
-    // Begin reading EEPROM on Raspberry Pi Pico
-    EEPROM.begin(512);
-  #endif
 
-  #if defined(MCU_IS_TEENSY) || defined(MCU_IS_RASPBERRY_PI_PICO_W)
+  // start reading from the first byte (address 0) of the EEPROM
+  unsigned int address = ALARM_ADDRESS_EEPROM;
+  // read a byte from the current address of the EEPROM
+  uint8_t value;
+  eeprom.eeprom_read(address, &value); address++;
+  if(value == 1) {
+    // alarm data is stored in EEPROM
+    // retrieve it
+    eeprom.eeprom_read(address, &alarmHr); address++;
+    eeprom.eeprom_read(address, &alarmMin); address++;
+    eeprom.eeprom_read(address, &alarmIsAm); address++;
+    eeprom.eeprom_read(address, &alarmOn); address++;
 
-    // start reading from the first byte (address 0) of the EEPROM
-    unsigned int address = ALARM_ADDRESS_EEPROM;
-    byte value;
-    // read a byte from the current address of the EEPROM
-    value = EEPROM.read(address);
-    if(value == 1) {
-      // alarm data is stored in EEPROM
-      // retrieve it
-      address++;
-      alarmHr = EEPROM.read(address); address++;
-      alarmMin = EEPROM.read(address); address++;
-      alarmIsAm = EEPROM.read(address); address++;
-      alarmOn = EEPROM.read(address); address++;
+    Serial.println(F("Alarm settings retrieved from EEPROM."));
 
-      #if defined(MCU_IS_RASPBERRY_PI_PICO_W)
-        // End reading EEPROM on Raspberry Pi Pico
-        EEPROM.end();
-      #endif
-
-      Serial.println(F("Alarm settings retrieved from EEPROM."));
-
-      return true;
-    }
-  
-    #if defined(MCU_IS_RASPBERRY_PI_PICO_W)
-      // End reading EEPROM on Raspberry Pi Pico
-      EEPROM.end();
-    #endif
-  #endif
+    return true;
+  }
 
   return false;
 }
 
 void persistent_data::saveAlarm(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn) {
 
-  #if defined(MCU_IS_TEENSY)
+  // start writing from the first byte of the EEPROM
+  unsigned int address = ALARM_ADDRESS_EEPROM;
+  // write alarm on EEPROM
+  if (!eeprom.eeprom_write(address, 1)) {
+    Serial.println("Failed to store 1");
+  }address++;
+  if (!eeprom.eeprom_write(address, alarmHr)) {
+    Serial.println("Failed to store alarmHr");
+  }address++;
+  if (!eeprom.eeprom_write(address, alarmMin)) {
+    Serial.println("Failed to store alarmMin");
+  }address++;
+  if (!eeprom.eeprom_write(address, alarmIsAm)) {
+    Serial.println("Failed to store alarmIsAm");
+  }address++;
+  if (!eeprom.eeprom_write(address, alarmOn)) {
+    Serial.println("Failed to store alarmOn");
+  }address++;
 
-    // start writing from the first byte of the EEPROM
-    unsigned int address = ALARM_ADDRESS_EEPROM;
-    // write alarm on EEPROM
-    EEPROM.update(address, 1); address++;
-    EEPROM.update(address, alarmHr); address++;
-    EEPROM.update(address, alarmMin); address++;
-    EEPROM.update(address, alarmIsAm); address++;
-    EEPROM.update(address, alarmOn);
-
-    Serial.println("Alarm written to EEPROM");
-
-  #elif defined(MCU_IS_RASPBERRY_PI_PICO_W)
-
-    // Begin reading EEPROM on Raspberry Pi Pico
-    EEPROM.begin(512);
-
-    // start writing from the first byte of the EEPROM
-    unsigned int address = ALARM_ADDRESS_EEPROM;
-    // write alarm on EEPROM
-    EEPROM.write(address, 1); address++;
-    EEPROM.commit();
-    EEPROM.write(address, alarmHr); address++;
-    EEPROM.commit();
-    EEPROM.write(address, alarmMin); address++;
-    EEPROM.commit();
-    EEPROM.write(address, alarmIsAm); address++;
-    EEPROM.commit();
-    EEPROM.write(address, alarmOn);
-    EEPROM.commit();
-
-    // End reading EEPROM on Raspberry Pi Pico
-    EEPROM.end();
-    Serial.println("Alarm written to EEPROM");
-
-  #endif
+  Serial.println(F("Alarm settings saved to EEPROM."));
 
 }
 
-void persistent_data::retrieveWiFiDetails(char* wifi_ssid, char* wifi_password) {
-  #if defined(MCU_IS_RASPBERRY_PI_PICO_W)
-    
-    // Begin reading EEPROM on Raspberry Pi Pico
-    EEPROM.begin(512);
-    
-    // read WiFi SSID and Password
-    char eeprom_read_array[WIFI_SSID_PASSWORD_LENGTH_MAX + 1];
+void persistent_data::retrieveWiFiDetails(char* &wifi_ssid, char* &wifi_password) {
 
-    // read wifi_ssid
-    int address = WIFI_ADDRESS_EEPROM;
-    int char_arr_start_address = WIFI_ADDRESS_EEPROM;
-    while(1) {
-      char eeprom_char_read = EEPROM.read(address);
-      eeprom_read_array[address - char_arr_start_address] = eeprom_char_read;
-      address++;
-      // break at null character
-      if(eeprom_char_read == '\0')
-        break;
-      // limit to force out of while loop, won't reach here in normal operation
-      if(address >= char_arr_start_address + WIFI_SSID_PASSWORD_LENGTH_MAX) {
-        eeprom_read_array[address - char_arr_start_address] = '\0';
-        break;
-      }
+  // read WiFi SSID and Password
+  char eeprom_read_array[WIFI_SSID_PASSWORD_LENGTH_MAX + 1];
+
+  // read wifi_ssid
+  int address = WIFI_ADDRESS_EEPROM;
+  int char_arr_start_address = WIFI_ADDRESS_EEPROM;
+  while(1) {
+    char eeprom_char_read;
+    eeprom.eeprom_read(address, &eeprom_char_read);
+    eeprom_read_array[address - char_arr_start_address] = eeprom_char_read;
+    address++;
+    // break at null character
+    if(eeprom_char_read == '\0')
+      break;
+    // limit to force out of while loop, won't reach here in normal operation
+    if(address >= char_arr_start_address + WIFI_SSID_PASSWORD_LENGTH_MAX) {
+      eeprom_read_array[address - char_arr_start_address] = '\0';
+      break;
     }
-    // fill wifi_ssid
-    if(wifi_ssid != NULL) {
-      delete wifi_ssid;
-      wifi_ssid = NULL;
+  }
+  // fill wifi_ssid
+  if(wifi_ssid != NULL) {
+    delete wifi_ssid;
+    wifi_ssid = NULL;
+  }
+  wifi_ssid = new char[address - char_arr_start_address];   // allocate space
+  strcpy(wifi_ssid,eeprom_read_array);
+
+  // read wifi_password
+  char_arr_start_address = address;
+  while(1) {
+    char eeprom_char_read;
+    eeprom.eeprom_read(address, &eeprom_char_read);
+    eeprom_read_array[address - char_arr_start_address] = eeprom_char_read;
+    address++;
+    // break at null character
+    if(eeprom_char_read == '\0')
+      break;
+    // limit to force out of while loop, won't reach here in normal operation
+    if(address >= char_arr_start_address + WIFI_SSID_PASSWORD_LENGTH_MAX) {
+      eeprom_read_array[address - char_arr_start_address] = '\0';
+      break;
     }
-    wifi_ssid = new char[address - char_arr_start_address];   // allocate space
-    strcpy(wifi_ssid,eeprom_read_array);
+  }
+  // fill wifi_password
+  if(wifi_password != NULL) {
+    delete wifi_password;
+    wifi_password = NULL;
+  }
+  wifi_password = new char[address - char_arr_start_address];   // allocate space
+  strcpy(wifi_password,eeprom_read_array);
 
-    // read wifi_password
-    char_arr_start_address = address;
-    while(1) {
-      char eeprom_char_read = EEPROM.read(address);
-      eeprom_read_array[address - char_arr_start_address] = eeprom_char_read;
-      address++;
-      // break at null character
-      if(eeprom_char_read == '\0')
-        break;
-      // limit to force out of while loop, won't reach here in normal operation
-      if(address >= char_arr_start_address + WIFI_SSID_PASSWORD_LENGTH_MAX) {
-        eeprom_read_array[address - char_arr_start_address] = '\0';
-        break;
-      }
-    }
-    // fill wifi_password
-    if(wifi_password != NULL) {
-      delete wifi_password;
-      wifi_password = NULL;
-    }
-    wifi_password = new char[address - char_arr_start_address];   // allocate space
-    strcpy(wifi_password,eeprom_read_array);
-
-    // End reading EEPROM on Raspberry Pi Pico
-    EEPROM.end();
-
-    Serial.println(F("WiFi details retrieved from EEPROM."));
-
-  #elif defined(MCU_IS_ESP32)
-
-    Serial.println(F("WiFi details retrieving on ESP32 is not Implemented yet."));
-
-  #endif
+  Serial.println(F("WiFi details retrieved from EEPROM."));
 }
+
 
 void persistent_data::saveWiFiDetails(char* wifi_ssid, char* wifi_password) {
-  #if defined(MCU_IS_RASPBERRY_PI_PICO_W)
-    // Begin reading EEPROM on Raspberry Pi Pico
-    EEPROM.begin(512);
 
-    // start writing from the first byte of the EEPROM
-    unsigned int address = WIFI_ADDRESS_EEPROM;
+  // start writing from the first byte of the EEPROM
+  unsigned int address = WIFI_ADDRESS_EEPROM;
 
-    // write wifi_ssid on EEPROM
-    int i = 0;
-    while(1) {
-      char c = *(wifi_ssid + i);
-      EEPROM.write(address, c); address++;
-      EEPROM.commit();
-      i++;
-      // break at null character
-      if(c == '\0')
-        break;
-      // limit to force out of while loop, won't reach here in normal operation
-      if(i >= WIFI_SSID_PASSWORD_LENGTH_MAX) {
-        EEPROM.write(address, '\0'); address++;
-        EEPROM.commit();
-        break;
-      }
+  // write wifi_ssid on EEPROM
+  int i = 0;
+  while(1) {
+    char c = *(wifi_ssid + i);
+    if (!eeprom.eeprom_write(address, c)) {
+      Serial.println("Failed to store c");
+    }address++;
+    i++;
+    // break at null character
+    if(c == '\0')
+      break;
+    // limit to force out of while loop, won't reach here in normal operation
+    if(i >= WIFI_SSID_PASSWORD_LENGTH_MAX) {
+      if (!eeprom.eeprom_write(address, '\0')) {
+        Serial.println("Failed to store '\0'");
+      }address++;
+      break;
     }
-    
-    // write wifi_password on EEPROM
-    i = 0;
-    while(1) {
-      char c = *(wifi_password + i);
-      EEPROM.write(address, c); address++;
-      EEPROM.commit();
-      i++;
-      // break at null character
-      if(c == '\0')
-        break;
-      // limit to force out of while loop, won't reach here in normal operation
-      if(i >= WIFI_SSID_PASSWORD_LENGTH_MAX) {
-        EEPROM.write(address, '\0'); address++;
-        EEPROM.commit();
-        break;
-      }
+  }
+  
+  // write wifi_password on EEPROM
+  i = 0;
+  while(1) {
+    char c = *(wifi_password + i);
+    if (!eeprom.eeprom_write(address, c)) {
+      Serial.println("Failed to store c");
+    }address++;
+    i++;
+    // break at null character
+    if(c == '\0')
+      break;
+    // limit to force out of while loop, won't reach here in normal operation
+    if(i >= WIFI_SSID_PASSWORD_LENGTH_MAX) {
+      if (!eeprom.eeprom_write(address, '\0')) {
+        Serial.println("Failed to store '\0'");
+      }address++;
+      break;
     }
+  }
 
-    // End reading EEPROM on Raspberry Pi Pico
-    EEPROM.end();
-
-    Serial.println(F("WiFi ssid and password written to EEPROM"));
-
-  #elif defined(MCU_IS_ESP32)
-
-    Serial.println(F("WiFi details saving on ESP32 is not Implemented yet."));
-
-  #endif
+  Serial.println(F("WiFi ssid and password written to EEPROM"));
 
 }
 
--- persistent_data.h
+++ persistent_data.h
@@ -2,23 +2,24 @@
 #define PERSISTENT_DATA_H
 
 #include <Arduino.h>
-#include "pin_defs.h"
-#if defined(MCU_IS_TEENSY) || defined(MCU_IS_RASPBERRY_PI_PICO_W)
-  #include <EEPROM.h>
-#endif
+#include "Wire.h"
+#include "uEEPROMLib.h"
 
 class persistent_data {
 
 public:
-
+  persistent_data();
   bool retrieveAlarmSettings(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn);
   void saveAlarm(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn);
-  void retrieveWiFiDetails(char* wifi_ssid, char* wifi_password);
+  void retrieveWiFiDetails(char* &wifi_ssid, char* &wifi_password);
   void saveWiFiDetails(char* wifi_ssid, char* wifi_password);
 
   const unsigned int WIFI_SSID_PASSWORD_LENGTH_MAX = 32;
 
 private:
+
+  // uEEPROMLib eeprom for AT24C32 EEPROM
+  uEEPROMLib eeprom;
 
   /** the address in the EEPROM **/
   const unsigned int ALARM_ADDRESS_EEPROM = 0; // stores data in order 0 = data is set, 1 = hr, 2 = min, 3 = isAm, 4 = alarmOn
