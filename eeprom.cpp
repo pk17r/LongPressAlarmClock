084ef7844d51bfb05800ba6aa5916cf2a3efa967
Prashant Kumar <prash.k20@gmail.com>
2024-02-23 18:13:27 -0800

    Made EEPROM class flexible for expansion

--- eeprom.cpp
+++ eeprom.cpp
@@ -1,3 +1,4 @@
+#include <sys/_stdint.h>
 #include "eeprom.h"
 #include "Wire.h"
 
@@ -8,143 +9,109 @@ EEPROM::EEPROM() {
   Wire.begin();
 
   // check if data is compatible with code, otherwise set the respective flag and default data
+  if(Fetch1Byte(kDataModelVersionAddress) != kDataModelVersion)
+    SaveDefaults();
   
   PrintLn("EEPROM Initialized!");
 }
 
-bool EEPROM::RetrieveAlarmSettings(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn) {
-
-  // start reading from the first byte (address 0) of the EEPROM
-  unsigned int address = kAlarmAddressEEPROM;
-  // read a byte from the current address of the EEPROM
-  uint8_t value;
-  eeprom_.eeprom_read(address, &value); address++;
-  if(value == 1) {
-    // alarm data is stored in EEPROM
-    // retrieve it
-    eeprom_.eeprom_read(address, &alarmHr); address++;
-    eeprom_.eeprom_read(address, &alarmMin); address++;
-    eeprom_.eeprom_read(address, &alarmIsAm); address++;
-    eeprom_.eeprom_read(address, &alarmOn); address++;
-
-    PrintLn("Alarm settings retrieved from EEPROM.");
-
-    return true;
-  }
-
-  PrintLn("Could not retrieve Alarm settings from EEPROM.");
-
-  return false;
+void EEPROM::SaveDefaults() {
+  Save1Byte(kDataModelVersionAddress, kDataModelVersion);
+  Save1Byte(kAlarmHrAddress, kAlarmHr);
+  Save1Byte(kAlarmMinAddress, kAlarmMin);
+  Save1Byte(kAlarmIsAmAddress, kAlarmIsAm);
+  Save1Byte(kAlarmOnAddress, kAlarmOn);
+  SaveString(kWiFiSsidLengthAddress, kWiFiSsidLengthMax, kWiFiSsidAddress, kWiFiSsid);
+  SaveString(kWiFiPasswdLengthAddress, kWiFiPasswdLengthMax, kWiFiPasswdAddress, kWiFiPasswd);
+  Save4Bytes(kWeatherZipCodeAddress, kWeatherZipCode);
+  Save1Byte(kWeatherCountryCodeAddress, static_cast<uint8_t>(kWeatherCountryCode[0]));
+  Save1Byte(kWeatherCountryCodeAddress + 1, static_cast<uint8_t>(kWeatherCountryCode[1]));
+  Save1Byte(kWeatherUnitsMetricNotImperialAddress, kWeatherUnitsMetricNotImperial);
+  PrintLn("Defaults saved to EEPROM!");
 }
 
-void EEPROM::SaveAlarm(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn) {
+bool EEPROM::RetrieveAlarmSettings(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn) {
+  alarmHr = Fetch1Byte(kAlarmHrAddress);
+  alarmMin = Fetch1Byte(kAlarmMinAddress);
+  alarmIsAm = static_cast<bool>(Fetch1Byte(kAlarmIsAmAddress));
+  alarmOn = static_cast<bool>(Fetch1Byte(kAlarmOnAddress));
+  return true;
+}
 
-  // start writing from the first byte of the EEPROM
-  unsigned int address = kAlarmAddressEEPROM;
-  // write alarm on EEPROM
-  if (!eeprom_.eeprom_write(address, 1)) {
-    PrintLn("Failed to store 1");
-  }address++;
-  if (!eeprom_.eeprom_write(address, alarmHr)) {
-    PrintLn("Failed to store alarmHr");
-  }address++;
-  if (!eeprom_.eeprom_write(address, alarmMin)) {
-    PrintLn("Failed to store alarmMin");
-  }address++;
-  if (!eeprom_.eeprom_write(address, alarmIsAm)) {
-    PrintLn("Failed to store alarmIsAm");
-  }address++;
-  if (!eeprom_.eeprom_write(address, alarmOn)) {
-    PrintLn("Failed to store alarmOn");
-  }address++;
-
-  PrintLn("Alarm settings saved to EEPROM.");
-
+void EEPROM::SaveAlarm(uint8_t alarmHr, uint8_t alarmMin, bool alarmIsAm, bool alarmOn) {
+  Save1Byte(kAlarmHrAddress, alarmHr);
+  Save1Byte(kAlarmMinAddress, alarmMin);
+  Save1Byte(kAlarmIsAmAddress, static_cast<uint8_t>(alarmIsAm));
+  Save1Byte(kAlarmOnAddress, static_cast<uint8_t>(alarmOn));
 }
 
 void EEPROM::RetrieveWiFiDetails(std::string &wifi_ssid, std::string &wifi_password) {
-
-  PrintLn("RetrieveWiFiDetails");
-  // read wifi_ssid
-  wifi_ssid = "";
-  int address = kWifiAddressEEPROM;
-  int char_arr_start_address = kWifiAddressEEPROM;
-  while(1) {
-    char eeprom_char_read;
-    eeprom_.eeprom_read(address, &eeprom_char_read);
-    address++;
-    // break at null character
-    if(eeprom_char_read == '\0')
-      break;
-    wifi_ssid = wifi_ssid + eeprom_char_read;
-    // limit to force out of while loop, won't reach here in normal operation
-    if(address >= char_arr_start_address + kWifiSsidPasswordLengthMax) {
-      break;
-    }
-  }
-  // PrintLn("EEPROM wifi_ssid: ", wifi_ssid);
-
-  // read wifi_password
-  wifi_password = "";
-  char_arr_start_address = address;
-  while(1) {
-    char eeprom_char_read;
-    eeprom_.eeprom_read(address, &eeprom_char_read);
-    address++;
-    // break at null character
-    if(eeprom_char_read == '\0')
-      break;
-    wifi_password = wifi_password + eeprom_char_read;
-    // limit to force out of while loop, won't reach here in normal operation
-    if(address >= char_arr_start_address + kWifiSsidPasswordLengthMax) {
-      break;
-    }
-  }
-  // PrintLn("EEPROM wifi_password: ", wifi_password);
-
+  wifi_ssid = FetchString(kWiFiSsidLengthAddress, kWiFiSsidAddress);
+  wifi_password = FetchString(kWiFiPasswdLengthAddress, kWiFiPasswdAddress);
+  PrintLn("EEPROM wifi_ssid: ", wifi_ssid);
+  PrintLn("EEPROM wifi_password: ", wifi_password);
   PrintLn("WiFi details retrieved from EEPROM.");
 }
 
 void EEPROM::SaveWiFiDetails(std::string wifi_ssid, std::string wifi_password) {
-
-  // start writing from the first byte of the EEPROM
-  unsigned int address = kWifiAddressEEPROM;
-
-  // write wifi_ssid on EEPROM
-  int i = 0;
-  while(1) {
-    char c = wifi_ssid[i];
-    if (!eeprom_.eeprom_write(address, c)) {
-      PrintLn("Failed to store c");
-    }address++;
-    i++;
-    // limit to force out of while loop, won't reach here in normal operation
-    if(i == kWifiSsidPasswordLengthMax || i == wifi_ssid.length()) {
-      if (!eeprom_.eeprom_write(address, '\0')) {
-        PrintLn("Failed to store '\0'");
-      }address++;
-      break;
-    }
-  }
-
-  // write wifi_password on EEPROM
-  i = 0;
-  while(1) {
-    char c = wifi_password[i];
-    if (!eeprom_.eeprom_write(address, c)) {
-      PrintLn("Failed to store c");
-    }address++;
-    i++;
-    // limit to force out of while loop, won't reach here in normal operation
-    if(i == kWifiSsidPasswordLengthMax || i == wifi_password.length()) {
-      if (!eeprom_.eeprom_write(address, '\0')) {
-        PrintLn("Failed to store '\0'");
-      }address++;
-      break;
-    }
-  }
-
+  SaveString(kWiFiSsidLengthAddress, kWiFiSsidLengthMax, kWiFiSsidAddress, wifi_ssid);
+  SaveString(kWiFiPasswdLengthAddress, kWiFiPasswdLengthMax, kWiFiPasswdAddress, wifi_password);
   PrintLn("WiFi ssid and password written to EEPROM");
 
 }
+
+std::string EEPROM::FetchString(uint16_t length_address, uint16_t data_address) {
+  int text_length = Fetch1Byte(length_address);
+  std::string text = "";
+  for (int i = 0; i < text_length; i++) {
+    text = text + static_cast<char>(Fetch1Byte(data_address + i));
+  }
+  return text;
+}
+
+void EEPROM::SaveString(uint16_t length_address, uint8_t length_max, uint16_t data_address, std::string text) {
+  Save1Byte(length_address, min(text.length(), length_max));
+  for (int i = 0; i < min(text.length(), length_max); i++) {
+    Save1Byte(data_address + i, static_cast<uint8_t>(text[i]));
+  }
+}
+
+
+uint8_t EEPROM::Fetch1Byte(uint16_t address) {
+  uint8_t value;
+  eeprom_.eeprom_read(address, &value);
+  return value;
+}
+
+void EEPROM::Save1Byte(uint16_t address, uint8_t value) {
+  if (!eeprom_.eeprom_write(address, value)) {
+    PrintLn("Failed to save to EEPROM!");
+  } 
+}
+
+uint32_t EEPROM::Fetch4Bytes(uint16_t address) {
+  uint32_t value = Fetch1Byte(address);   // MSB
+  value = value << 8;
+  value = value & Fetch1Byte(address + 1);
+  value = value << 8;
+  value = value & Fetch1Byte(address + 2);
+  value = value << 8;
+  value = value & Fetch1Byte(address + 3);    // LSB
+  return value;
+}
+
+void EEPROM::Save4Bytes(uint16_t address, uint32_t value) {
+  uint8_t val8bit = value & 0xff; // LSB
+  Save1Byte(address + 3, val8bit);
+  value = value >> 8;
+  val8bit = value & 0xff;
+  Save1Byte(address + 2, val8bit);
+  value = value >> 8;
+  val8bit = value & 0xff;
+  Save1Byte(address + 1, val8bit);
+  value = value >> 8;
+  val8bit = value & 0xff;
+  Save1Byte(address, val8bit);   // MSB
+}
+
 
--- eeprom.h
+++ eeprom.h
@@ -1,3 +1,4 @@
+#include <sys/_stdint.h>
 #ifndef EEPROM_H
 #define EEPROM_H
 
@@ -9,11 +10,19 @@ class EEPROM {
 public:
   EEPROM();
   bool RetrieveAlarmSettings(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn);
-  void SaveAlarm(uint8_t &alarmHr, uint8_t &alarmMin, bool &alarmIsAm, bool &alarmOn);
+  void SaveAlarm(uint8_t alarmHr, uint8_t alarmMin, bool alarmIsAm, bool alarmOn);
   void RetrieveWiFiDetails(std::string &wifi_ssid, std::string &wifi_password);
   void SaveWiFiDetails(std::string wifi_ssid, std::string wifi_password);
 
 private:
+
+  void SaveDefaults();
+  std::string FetchString(uint16_t length_address, uint16_t data_address);
+  void SaveString(uint16_t length_address, uint8_t length_max, uint16_t data_address, std::string text);
+  uint8_t Fetch1Byte(uint16_t address);
+  void Save1Byte(uint16_t address, uint8_t value);
+  uint32_t Fetch4Bytes(uint16_t address);
+  void Save4Bytes(uint16_t address, uint32_t value);
 
   // uEEPROMLib eeprom for AT24C32 EEPROM
   uEEPROMLib eeprom_;
@@ -25,6 +34,41 @@ private:
   const unsigned int kAlarmAddressEEPROM = 0; // stores data in order 0 = data is set, 1 = hr, 2 = min, 3 = isAm, 4 = alarmOn
 
   const unsigned int kWifiAddressEEPROM = 5; // stores data in order 5 = wifi_ssid ending with \0 thereafter wifi_password ending with \0
+
+
+  // ADDRESSES
+
+  const uint8_t kDataModelVersion = 101;
+  const uint16_t kDataModelVersionAddress = 0;
+  const uint16_t kAlarmHrAddress = 1;
+  const uint16_t kAlarmMinAddress = 2;
+  const uint16_t kAlarmIsAmAddress = 3;
+  const uint16_t kAlarmOnAddress = 4;
+  const uint8_t kWiFiSsidLengthAddress = 5;
+  const uint8_t kWiFiSsidLengthMax = kWifiSsidPasswordLengthMax;
+  const uint16_t kWiFiSsidAddress = 6;
+  const uint16_t kWiFiPasswdLengthAddress = 38;
+  const uint8_t kWiFiPasswdLengthMax = kWifiSsidPasswordLengthMax;
+  const uint16_t kWiFiPasswdAddress = 39;
+  const uint16_t kWeatherZipCodeAddress = 71;
+  const uint8_t kWeatherZipCodeBytes = 4;
+  const uint16_t kWeatherCountryCodeAddress = 75;
+  const uint8_t kWeatherCountryCodeBytes = 2;
+  const uint16_t kWeatherUnitsMetricNotImperialAddress = 77;
+
+
+  // DEFAULTS
+
+  const uint8_t kAlarmHr = 7;
+  const uint8_t kAlarmMin = 30;
+  const bool kAlarmIsAm = true;
+  const bool kAlarmOn = false;
+  const std::string kWiFiSsid = "Enter SSID";
+  const std::string kWiFiPasswd = "Enter Passwd";
+  const uint32_t kWeatherZipCode = 92104;
+  const std::string kWeatherCountryCode = "US";
+  const bool kWeatherUnitsMetricNotImperial = false;
+
 
 };
 
