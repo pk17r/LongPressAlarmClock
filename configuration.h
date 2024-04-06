78fcc3d93bd498ef23c8cb39b765db1debcefc52
Prashant Kumar <prash.k20@gmail.com>
2024-04-06 16:23:00 -0700

    Version update and binaries

--- configuration.h
+++ configuration.h
@@ -5,8 +5,8 @@
 
 // SELECT MCU
 
-#define MCU_IS_ESP32_S2_MINI
-// #define MCU_IS_ESP32_WROOM_DA_MODULE
+// #define MCU_IS_ESP32_S2_MINI
+#define MCU_IS_ESP32_WROOM_DA_MODULE
 // #define MCU_IS_RASPBERRY_PI_PICO_W
 // #define MCU_IS_ESP32_S3
 
@@ -31,12 +31,12 @@
 
 // FIRMWARE VERSION   (update these when pushing new MCU specific binaries to github)
 
-#define ESP32_S2_MINI_FIRMWARE_VERSION            "2.1"
-#define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "2.1"
+#define ESP32_S2_MINI_FIRMWARE_VERSION            "2.2"
+#define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "2.2"
 #define RASPBERRY_PI_PICO_W_FIRMWARE_VERSION      "1.5"
-#define ESP32_S3_FIRMWARE_VERSION                 "2.1"
-const std::string kFirmwareDate = "Apr 5, 2024";
+#define ESP32_S3_FIRMWARE_VERSION                 "2.2"
+const std::string kFirmwareDate = "Apr 6, 2024";
 
-const std::string kChangeLog = "- Screensaver Settings\n- Flexible Page Structure\n- Photoresistor added";
+const std::string kChangeLog = "- Screensaver Settings\n- Flexible Page Structure\n- NightTime Setting";
 
 #endif  // CONFIGURATION_H
