0efe6bd6bac5a187219bced1e0f7e0f1d508876f
Prashant Kumar <prash.k20@gmail.com>
2024-04-19 23:05:00 -0700

    v2.3 for ESP32 WROOM DA Module

--- configuration.h
+++ configuration.h
@@ -5,8 +5,8 @@
 
 // SELECT MCU
 
-#define MCU_IS_ESP32_S2_MINI
-// #define MCU_IS_ESP32_WROOM_DA_MODULE
+// #define MCU_IS_ESP32_S2_MINI
+#define MCU_IS_ESP32_WROOM_DA_MODULE
 // #define MCU_IS_RASPBERRY_PI_PICO_W
 // #define MCU_IS_ESP32_S3
 
@@ -32,7 +32,7 @@
 // FIRMWARE VERSION   (update these when pushing new MCU specific binaries to github)
 
 #define ESP32_S2_MINI_FIRMWARE_VERSION            "2.3"
-#define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "2.2"
+#define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "2.3"
 #define RASPBERRY_PI_PICO_W_FIRMWARE_VERSION      "1.5"
 #define ESP32_S3_FIRMWARE_VERSION                 "2.3"
 const std::string kFirmwareDate = "Apr 19, 2024";
--- pin_defs.h
+++ pin_defs.h
@@ -78,6 +78,7 @@
   const int BUZZER_PIN = 13;
   const int DEBUG_PIN = 12;    // manually pull down to enable debug mode, watchdog reboot will not be used in debug mode
   const int PHOTORESISTOR_PIN = 25;
+  const int RGB_LED_STRIP_PIN = 5;
 
 
 
