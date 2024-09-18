433ffcff0ee8dfa0117fcca1b70011cd07fcb8a9
Prashant Kumar <prash.k20@gmail.com>
2024-09-18 01:23:30 -0700

    Binaries for S3

--- configuration.h
+++ configuration.h
@@ -5,10 +5,10 @@
 
 // SELECT MCU
 
-#define MCU_IS_ESP32_S2_MINI
+// #define MCU_IS_ESP32_S2_MINI
 // #define MCU_IS_ESP32_WROOM_DA_MODULE
 // #define MCU_IS_RASPBERRY_PI_PICO_W
-// #define MCU_IS_ESP32_S3
+#define MCU_IS_ESP32_S3
 
 
 // SELECT DISPLAY
@@ -29,7 +29,7 @@
 #define ESP32_S2_MINI_FIRMWARE_VERSION            "2.5"
 #define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "2.4"
 #define RASPBERRY_PI_PICO_W_FIRMWARE_VERSION      "1.5"
-#define ESP32_S3_FIRMWARE_VERSION                 "2.4"
+#define ESP32_S3_FIRMWARE_VERSION                 "2.5"
 const std::string kFirmwareDate = "Sep 15, 2024";
 
 const std::string kChangeLog = "- RGB LEDs Settings\n- WiFi & Location Settings\n  Flow enhancements.";
