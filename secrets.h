1b69797544a04bfbe169ed9e9330d6786e6c7774
Prashant Kumar <prash.k20@gmail.com>
2024-11-03 10:03:24 -0800

    Add commands to stop tracking secrets.h in local repo

--- secrets.h
+++ secrets.h
@@ -1,6 +1,19 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
+/*  ADD LOCAL CHANGES TO THIS FILE WITHOUT UPDATING GIT REPOSITORY
+
+This is to tell Git that you want your own independent version of this file,
+for instance, you don't want to overwrite this file in git repository, use:
+
+    git update-index --skip-worktree secrets.h
+
+To undo the above action, use:
+
+    git update-index --no-skip-worktree secrets.h
+
+*/
+
 // Enter your WiFi SSID and Password to not manually fill in later
 #define MY_WIFI_SSID "SSID"
 #define MY_WIFI_PASSWD "Password"
