b91b3f5480f99956ce3218fd53ae44cde1691403
Prashant Kumar <prash.k20@gmail.com>
2024-11-04 21:58:42 -0800

    Add binaries for ESP32 S2 v2.7 update

--- configuration.h
+++ configuration.h
@@ -5,10 +5,10 @@
 
 // SELECT MCU
 
-// #define MCU_IS_ESP32_S2_MINI
+#define MCU_IS_ESP32_S2_MINI
 // #define MCU_IS_ESP32_WROOM_DA_MODULE
 // #define MCU_IS_RASPBERRY_PI_PICO_W
-#define MCU_IS_ESP32_S3
+// #define MCU_IS_ESP32_S3
 
 
 // SELECT DISPLAY
@@ -27,12 +27,12 @@
 
 // FIRMWARE VERSION   (update these when pushing new MCU specific binaries to github)
 
-#define ESP32_S2_MINI_FIRMWARE_VERSION            "2.5"
+#define ESP32_S2_MINI_FIRMWARE_VERSION            "2.7"
 #define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "2.4"
 #define RASPBERRY_PI_PICO_W_FIRMWARE_VERSION      "1.5"
 #define ESP32_S3_FIRMWARE_VERSION                 "2.7"
 const std::string kFirmwareDate = "Nov 4, 2024";
 
-const std::string kChangeLog = "- Touchscreen Input!";
+const std::string kChangeLog = "- Auto Time Update Improved\n- LED Brightness Adjustable\n- WiFi Scan & Selection";
 
 #endif  // CONFIGURATION_H
